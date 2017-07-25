5cbdd86411277d9a73d5e3e1dbd77cd3cd86872c
Damodar Shenoy <dshenoy@vmware.com>
2017-07-25 21:24:44 +0530

    Logic to add multi line comment

--- MultilineCommentExt/SourceEditorCommand.swift
+++ MultilineCommentExt/SourceEditorCommand.swift
@@ -12,6 +12,24 @@ import XcodeKit
 class SourceEditorCommand: NSObject, XCSourceEditorCommand {
     
     func perform(with invocation: XCSourceEditorCommandInvocation, completionHandler: @escaping (Error?) -> Void ) -> Void {
+        let lines = invocation.buffer.lines
+        let selectedRange = invocation.buffer.selections
+        
+        let textRange: XCSourceTextRange? = selectedRange[0] as? XCSourceTextRange
+        if let txtRng = textRange {
+
+            var startLine: String = lines[txtRng.start.line] as! String
+            startLine.insert("*", at: startLine.startIndex)
+            startLine.insert("/", at: startLine.startIndex)
+            
+            lines[txtRng.start.line] = startLine
+            
+            var endLine: String = lines[txtRng.end.line] as! String
+            endLine.insert("*", at: endLine.endIndex)
+            endLine.insert("/", at: endLine.endIndex)
+            
+            lines[txtRng.end.line] = endLine
+        }
         
         completionHandler(nil)
     }
